11d2863d66c5d16f75f835b93af87a40c4d429dd
syc7033 <18945449295@163.com>
2024-10-18 16:49:00 +0800

    提交代码main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void sort(vector<int>& vec)
+{
+
+}
+
+int main()
+{
+    vector<int> vec = {5, 3, 1, 4, 2};
+    sort(vec);
+    return 0;
+}
